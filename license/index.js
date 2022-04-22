553caafafc2f23a2a452b0811d09d868ebd1c7de
Kevin Pang <moistbobo@gmail.com>
2022-04-22 14:40:32 +0800

    feat(License): Add Apache-2.0 to license property in packagejson

--- license/index.js
+++ license/index.js
@@ -1,5 +1,6 @@
 const {
-    copyFiles
+    copyFiles,
+    packageJson
 } = require('mrm-core');
 
 const {
@@ -10,8 +11,12 @@ const licenseFilePath = `${baseFilePath}/license`
 
 function task() {
     copyFiles(licenseFilePath, ['LICENSE']);
+
+    packageJson()
+        .set('License', 'Apache-2.0')
+        .save();
 }
 
-module.exports.description = "Adds Forbole's Apache 2.0 license to the project.";
+module.exports.description = "Adds Apache 2.0 License file and Apache-2.0 to the license field in package.json";
 
 module.exports = task;
