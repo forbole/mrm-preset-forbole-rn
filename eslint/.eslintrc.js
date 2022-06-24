d7ab48b9791124953425a4db465d077995058407
Kevin Pang <moistbobo@gmail.com>
2022-06-24 20:57:48 +0800

    refactor(eslint): fix formatting

--- eslint/.eslintrc.js
+++ eslint/.eslintrc.js
@@ -1,51 +1,36 @@
 module.exports = {
-  "env": {
-    "es6": true,
-    "jest": true
+  env: {
+    es6: true,
+    jest: true,
   },
-  "extends": [
-    "airbnb",
-    "@react-native-community"
-  ],
-  "parser": "@typescript-eslint/parser",
-  "plugins": [
-    "@typescript-eslint",
-    "react-native"
-  ],
-  "globals": {
-    "window": true,
-    "fetch": false
+  extends: ['airbnb', '@react-native-community'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react-native'],
+  globals: {
+    window: true,
+    fetch: false,
   },
-  "rules": {
-    "react/jsx-filename-extension": [
+  rules: {
+    'react/jsx-filename-extension': [
       1,
       {
-        "extensions": [
-          ".jsx",
-          ".tsx"
-        ]
-      }
-    ],
-    "import/extensions": 0,
-    "import/no-extraneous-dependencies": 0,
-    "react/jsx-props-no-spreading": 0,
-    "react-hooks/exhaustive-deps": 0,
-    "react/require-default-props": 0,
-    "react/function-component-definition": 0,
-    "react-native/sort-styles": "error"
-  },
-  "settings": {
-    "import/resolver": {
-      "node": {
-        "extensions": [
-          ".js",
-          ".jsx",
-          ".ts",
-          ".tsx",
-          ".json"
-        ]
+        extensions: ['.jsx', '.tsx'],
       },
-      "typescript": {}
-    }
-  }
-}
+    ],
+    'import/extensions': 0,
+    'import/no-extraneous-dependencies': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react-hooks/exhaustive-deps': 0,
+    'react/require-default-props': 0,
+    'react/function-component-definition': 0,
+    'react-native/sort-styles': 'error',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+      },
+      typescript: {},
+    },
+  },
+};
